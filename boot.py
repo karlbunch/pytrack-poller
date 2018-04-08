ac4801066b8f414fbff3408e4ae01561d3b293d5
Karl Bunch <karlbunch@karlbunch.com>
2018-04-08 17:50:06 -0400

    Use heartbeat to show status of wifi setup

--- boot.py
+++ boot.py
@@ -5,18 +5,30 @@ def boot_wifi(ssid, password):
     import network
     import time
     import gc
+    import pycom
 
     print(">> boot.py: START")
 
     wlan = network.WLAN(mode=network.WLAN.STA)
 
-    if not wlan.isconnected():
-        wlan.connect(ssid, auth=(network.WLAN.WPA2, password))
+    pycom.heartbeat(False)
+    pycom.rgbled(0x090000)
 
-    while not wlan.isconnected():
+    wlan_timeout = time.time() + 15
+
+    if not wlan.isconnected():
+        wlan.connect(ssid, auth=(network.WLAN.WPA2, password), timeout=10000)
+
+    colors = [ 0x000009, 0x000900, 0x090000 ]
+
+    while not wlan.isconnected() and time.time() < wlan_timeout:
+        pycom.rgbled(colors[time.time() % len(colors)])
         time.sleep(1)
 
-    print(">> boot.py: IP: {:,}".format(wlan.ifconfig()[0]))
+    try:
+        print(">> boot.py: IP: {:,}".format(wlan.ifconfig()[0]))
+    except:
+        pass
 
     rtc = machine.RTC()
 
@@ -29,10 +41,15 @@ def boot_wifi(ssid, password):
 
     print(">> boot.py: Time: %04d/%02d/%02d %02d:%02d:%02d UTC" % time.localtime()[:6])
 
-    gc.collect()
-
-    print(">> boot.py: END - free = {}".format(gc.mem_free()))
+    pycom.heartbeat(False)
 
     gc.collect()
+
+    print(">> boot.py: gc.mem_free = {}".format(gc.mem_free()))
+
+    machine.info()
+    gc.collect()
+
+    print(">> boot.py: END")
 
 boot_wifi(config.WIFI_SSID, config.WIFI_PASSWORD)
